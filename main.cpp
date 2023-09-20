4583cd128cc8f22a4e29f42ad7028143dda8db4c
Alexander Platonov <144921734+sashokdimasik@users.noreply.github.com>
2023-09-20 18:04:35 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+/***********************************
+ * Author:       Platonov A.A.     *
+ * Group:        ПИ-231            *
+ * Variant:      7                 *
+ * Date:         13.09.2023        *
+ * Project name: Fatass Matrix Row *
+ ***********************************/
+
+#include <iostream>
+
+int main()
+{
+  int width, height;
+  
+  std::cout << "Enter width & height of the matrix C: ";
+  std::cin >> width >> height;
+  
+  double **matrixC = new double*[width];
+  
+  std::cout << "Enter matrix C:\n";
+  
+  for (int i = 0; i < width; i++) {
+    matrixC[i] = new double[height];
+    for (int j = 0; j < height; j++) {
+      std::cin >> matrixC[i][j];
+    }
+  }
+  
+  double sumCurr, sumMax = 0;
+  int rowMax = 1;
+  
+  for (int i = 0; i < width; i++) {
+    sumCurr = 0;
+    for (int j = 0; j < height; j++) {
+      sumCurr += matrixC[i][j];
+    }
+    if (sumCurr > sumMax) {
+      sumMax = sumCurr;
+      rowMax = i;
+    }
+  }
+  
+  std::cout << "Matrix C:\n";
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      std::cout << matrixC[i][j] << ' ';
+    }
+    std::cout << '\n';
+  }
+  
+  std::cout << "Row with max sum:\n";
+  
+  for (int i = 0; i < height; i++) {
+    std::cout << matrixC[rowMax][i] << ' ';
+  }
+  
+  std::cout << "\nSum = " << sumMax << '\n';
+  
+  return 0;
+}
